610fd6f0f0f1cd54ba9f63a125dd2070ba8310b0
Yassine Belmamoun <belmamoun.yassine@gmail.com>
2025-05-21 22:44:56 +0700

    fix symlink

--- .spaceshiprc.zsh
+++ .spaceshiprc.zsh
@@ -1,1 +1,25 @@
-/Users/yassinebelmamoun/.spaceshiprc.zsh
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Add custom Ember section
+# See: https://github.com/spaceship-prompt/spaceship-ember
+spaceship add ember
+
+# Add a custom vi-mode section to the prompt
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+spaceship add --before char vi_mode
+
+# Kubernetes
+SPACESHIP_KUBECTL_SHOW=true
+SPACESHIP_KUBECTL_VERSION_SHOW=false
+SPACESHIP_KUBECTL_PREFIX="using "
+SPACESHIP_KUBECTL_SUFFIX=" "
+SPACESHIP_KUBECTL_COLOR="cyan"
+SPACESHIP_KUBECTL_SYMBOL="☸️·"
+SPACESHIP_KUBECTL_CONTEXT_SHOW_NAMESPACE=true
